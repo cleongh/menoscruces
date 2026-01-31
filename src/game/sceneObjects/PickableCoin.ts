95220993769c6bf285341a633bd8c11253efe0c3
Pablo Gutiérrez Sánchez <pabgut02@ucm.es>
2026-01-31 14:17:02 +0100

    gestión de grasa

--- src/game/sceneObjects/PickableCoin.ts
+++ src/game/sceneObjects/PickableCoin.ts
@@ -1,25 +1,15 @@
 import { AbstractCoin } from "./AbstractCoin";
 
 export class PickableCoin extends AbstractCoin {
-  public coinData: { texture: string; value: number; stat: string };
-
-  constructor(
-    scene: Phaser.Scene,
-    x: number,
-    y: number,
-    texture: string,
-    value: number,
-    stat: string,
-  ) {
+  constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
     super(scene, x, y, texture, 20);
-    this.coinData = { texture, value, stat };
 
     // TODO: tunear cuerpo para que se sienta natural al chocar
     this.body.setCircle(this.width / 2);
   }
 
   public handleCoinPickup(): void {
-    this.scene.events.emit("coin-collected", this.coinData);
+    this.scene.events.emit("coin-collected");
     this.destroy();
   }
 }
