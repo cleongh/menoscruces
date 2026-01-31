f74a454dbd0306ceb8335ac286cbb5add0c823b5
gorco <antcal01@ucm.es>
2026-01-31 18:25:35 +0100

    probando eso de las monedas

--- src/game/sceneObjects/coinDefinitions.ts
+++ src/game/sceneObjects/coinDefinitions.ts
@@ -11,7 +11,7 @@ export const coinDefinitions: TierDefinition[] = [
     availableCoins: [
       {
         texture: "big-coin",
-        name: "Moneda Vital",
+        name: "Snake Coin",
         passCost: 55,
         option1: {
           name: "+10 Extra Health",
@@ -46,6 +46,43 @@ export const coinDefinitions: TierDefinition[] = [
           },
         },
       },
+      {
+        texture: "big-coin",
+        name: "Ares Coin",
+        passCost: 85,
+        option1: {
+          name: "+1 Attack",
+          description: "Increases your attack by 1 point.",
+          kind: "passive",
+          texture: "coin",
+          modifier: (state) => {
+            const newAttack = state.baseStats.attackBase + 1;
+            return {
+              ...state,
+              baseStats: {
+                ...state.baseStats,
+                attackBase: newAttack,
+              },
+            };
+          },
+        },
+        option2: {
+          name: "-1 Attack",
+          description: "Decreases your attack by 1 point.",
+          kind: "passive",
+          texture: "coin",
+          modifier: (state) => {
+            const newAttack = state.baseStats.attackBase - 1;
+            return {
+              ...state,
+              baseStats: {
+                ...state.baseStats,
+                attackBase: newAttack,
+              },
+            };
+          },
+        },
+      }
     ],
   },
 ];
