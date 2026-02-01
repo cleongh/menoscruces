0eb9f35e22331ed9cb9a229d33184c20dfa8542f
AnaR <fearlessann+gh@gmail.com>
2026-02-01 15:08:11 +0100

    colorinchi

--- src/game/UI/colors.ts
+++ src/game/UI/colors.ts
@@ -2,3 +2,15 @@ export const colors = {
   head: 0x3d6dc2,
   tail: 0xc2923d,
 };
+
+export const basicColors = {
+  yellow: "#ffe588", 
+  blue: "#9fc8e8", 
+  teal: "#67e6d1",
+  green: "#82eb8e", 
+  pink: "#eda1b0", 
+  purple: "#c89edc", 
+  cream: "#f1d6be",
+  bone: "#f4ece6"
+
+}
